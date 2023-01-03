d41091d18187d84abb012f9a3c6e4d16a5ab4339
Rajan Gupta <118849722+rajangupta04@users.noreply.github.com>
2023-01-03 18:11:13 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,7 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import imgage from '../components/signup_img.svg'
+
 
 const Signup = () => {
 
@@ -9,22 +12,28 @@ const Signup = () => {
     }
 
   return (
+    
     <div className="signup"> 
-        <h1>Wheel For Money </h1>
-        <form onSubmit={handleSubmit}>
-          <h2>Create a new account</h2>
+        {/* <h2>Wheel For Money </h2> */}
+          <div className="img">
+            <img src={imgage} alt="No image found" width="460px" height="520px"/>
+          </div>
 
-            <label>First Name:</label> <input type="text" placeholder="First Name"/> 
-           <label>Last Name:</label>  <input type="text" placeholder="Last Name" />
-            <label>Email:</label>      <input type="email" placeholder="Email" />
-            <label>Password:</label>   <input type="text" placeholder="Password" />
-            <label>Confirm Password:</label>  <input type="text" placeholder="Confirm Password" />
-            
-            <input type="submit" value="Create Account" />
-            <br/ >
-            <div className="login-link"> <Link to="/Login.jsx"> Already have an account?  </Link> </div>
-            
-        </form>
+          <div className="form">
+              <form onSubmit={handleSubmit}>
+                <h1>Create a new account</h1>
+                <div className="form_input">
+                    <input type="text" placeholder="First Name"/> 
+                    <input type="text" placeholder="Last Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="text" placeholder="Password" />
+                    <input type="text" placeholder="Confirm Password" /> <br/>
+                    <input type="submit" value="Create Account" />
+                    <br/ >
+                </div>
+                <Link to="/Login.jsx"> <h3> Already have an account? </h3> </Link> 
+              </form>
+          </div>
     </div>
   );
 }
